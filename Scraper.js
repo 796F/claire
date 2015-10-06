288589b67af7204f3794333e975a15124117e423
Michael Xia <mxia.mit@gmail.com>
2015-10-06 10:47:51 -0700

    working, save state

--- Scraper.js
+++ Scraper.js
@@ -6,18 +6,19 @@ var config = require('./config.js');
 // githubArchiveStream.init(config);
 // githubArchiveStream.run();
 
-var newsletterStream = require('./lib/email');
-newsletterStream.init(config);
-newsletterStream.run();
+// var newsletterStream = require('./lib/email');
+// newsletterStream.init(config);
+// newsletterStream.run();
 
 // var ProxyFactory = require('nonproxy').init(config.github_auth)
 // // var ProxyFactory = require('ec2factory').init(config.ec2)
-// // var ProxyFactory = require('torfactory').init(config.tor)
-// .then(function(ready_factory){
-//   //factory ready, start up
-//   Github.run({
-//     db_connector: knex,
-//     proxy_factory: ready_factory,
-//     github_auth : config.github_auth
-//   });
-// });
+var ProxyFactory = Proxies.torfactory.init(config.tor)
+.then(function(ready_factory){
+  debugger;
+  //factory ready, start up
+  // Github.run({
+  //   db_connector: knex,
+  //   proxy_factory: ready_factory,
+  //   github_auth : config.github_auth
+  // });
+});
